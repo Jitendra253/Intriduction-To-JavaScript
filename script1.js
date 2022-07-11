38cc0a624bb5dcd74fe227dc4614395ea15cf0ea
Jitendra Kumar Mahali <jitendramahali253@gmail.com>
2022-07-11 22:28:24 +0530

    initial commit

---
+++ script1.js
@@ -0,0 +1,22 @@
+// accessing the HTML elements and adding texts to it
+document.getElementById('question').innerHTML = "1.what is JavaScript?" 
+document.getElementById('answer').innerHTML = "Javascript is used by programmers across the world to create dynamic and interactive web content like applications and browsers. JavaScript is so popular that it's the most used programming language in the world, used as a client-side programming language by 97.0% of all websites.";
+
+// accessing the heading and adding styles to it
+document.getElementById('question').style.color = 'red';
+document.getElementById('question').style.fontSize = '45px'
+document.getElementById('question').style.margin = '0px';
+
+
+// accessing the paragraph and adding styles to it
+
+document.getElementById('answer').style.color = 'blue';
+document.getElementById('answer').style.fontSize = '25px';
+
+// styling background
+document.body.style.backgroundColor = "lightpink";
+document.body.style.margin = "5rem";
+
+
+
+
